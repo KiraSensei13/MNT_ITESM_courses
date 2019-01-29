844ad0d34242cd36d313274787276608d09a2e52
Osamu <oskatagiri@gmail.com>
2019-01-29 13:44:00 -0600

    R HW - linear, polinomial & anova regressions

--- 1.1.Computacion_Aplicada/02_LinearRegression/Homework2/Homework2.R
+++ 1.1.Computacion_Aplicada/02_LinearRegression/Homework2/Homework2.R
@@ -17,13 +17,15 @@
 #*     XX Feb 2019
 #************************************************************************
 
-
+#install.packages('e1071', dependencies=TRUE)
+#install.packages('caret', dependencies=TRUE)
 
 #*** PART 1 *************************************************************
 
 # Do a regression analysis to the “pima” dataset from the “faraway”
 # library
 library(faraway)
+library(caret)
 data(pima)
 
 # * Analyze the database and select only the observations with no missing
@@ -34,21 +36,95 @@ completeData = complete.cases(pima)
 # remove rows with incomplete data
 mydata = pima[completeData, ]
 
+#Let's take a look to the data ...
+str(mydata)
+pairs(mydata)
+cor(mydata)
+summary(mydata)
+
 # * Use the R function to fit a model.
 # * Dependent variable : Test -> test is a categorical variable ...
 
-#ANCOVA Analysis
-#Model with interaction between categorical variable and predictor variable
-# Get the dataset.
-input <- mydata
-
 # Create the regression model.
-head(mydata)
-result <- aov(test~pregnant+glucose+diastolic+triceps+insulin+bmi+diabetes+age+test,data = input)
-print(summary(result))
+#names(mydata)
+
+#------------------------------------------------------------------------
+
+#LINEAR ANALYSIS
+# lmFitModel <- lm(test ~ pregnant + glucose + diastolic + triceps + insulin + bmi + diabetes + age, data = mydata)
+# summary(lmFitModel)
+# lmFitModel
+#Let's remove triceps, insulin, and age as they do not have a significant impact on the model
+lmFitModel <- lm(test ~ pregnant + glucose + diastolic + bmi + diabetes, data = mydata)
+summary(lmFitModel)
+lmFitModel
+
+#------------------------------------------------------------------------
+
+#POLYNOMIAL ANALYSIS
+gp6Model = lm(
+  test ~
+      (pregnant + glucose + diastolic + bmi + diabetes)    +
+    I((pregnant + glucose + diastolic + bmi + diabetes)^2) +
+    I((pregnant + glucose + diastolic + bmi + diabetes)^3) +
+    I((pregnant + glucose + diastolic + bmi + diabetes)^4) +
+    I((pregnant + glucose + diastolic + bmi + diabetes)^5) +
+    I((pregnant + glucose + diastolic + bmi + diabetes)^6),
+  data = mydata)
+summary(gp6Model)
+gp6Model
+
+#------------------------------------------------------------------------
+
+#ANOVA ANALYSIS
+# aovFitModel <- aov(test ~ pregnant + glucose + diastolic + triceps + insulin + bmi + diabetes + age, data = mydata)
+# summary(aovFitModel)
+# aovFitModel
+#Let's remove diastolic, triceps, and insulin as they do not have a significant impact on the model
+aovFitModel <- aov(test ~ pregnant + glucose + bmi + diabetes + age, data = mydata)
+summary(aovFitModel)
+aovFitModel
+
+#------------------------------------------------------------------------
 
 # * How many correct predictions did the fitted model get? How many wrong?
+
+#get test predictions using the LINEAR ANALYSIS
+#We assume that: all values less than 0.5 correspond to the first category,
+#0.5 or bigger correspond to the second category
+lm_predictions = ifelse(predict(lmFitModel, mydata) > 0.5, "1", "0" )
+#compare the actual data agains the predictions
+correct_lm_predictions = mydata$test == lm_predictions
+#count how many of the predictions are right vs wrong
+#FALSE:WRONG TRUE:RIGHT
+summary(correct_lm_predictions)
 # * Confusion matrix
+confusionMatrix(as.factor(lm_predictions), as.factor(mydata$test), positive = NULL, dnn = c("Prediction", "Reference"))
+
+#get test predictions using the POLYNOMIAL ANALYSIS
+#We assume that: all values less than 0.5 correspond to the first category,
+#0.5 or bigger correspond to the second category
+gp6_predictions = ifelse(predict(gp6, mydata) > 0.5, "1", "0" )
+#compare the actual data agains the predictions
+correct_gp6_predictions = mydata$test == gp6_predictions
+#count how many of the predictions are right vs wrong
+#FALSE:WRONG TRUE:RIGHT
+summary(correct_gp6_predictions)
+# * Confusion matrix
+confusionMatrix(as.factor(gp6_predictions), as.factor(mydata$test), positive = NULL, dnn = c("Prediction", "Reference"))
+
+#get test predictions using the ANOVA ANALYSIS
+#We assume that: all values less than 0.5 correspond to the first category,
+#0.5 or bigger correspond to the second category
+aov_predictions = ifelse(predict(aovFitModel, mydata) > 0.5, "1", "0" )
+#compare the actual data agains the predictions
+correct_aov_predictions = mydata$test == aov_predictions
+#count how many of the predictions are right vs wrong
+#FALSE:WRONG TRUE:RIGHT
+summary(correct_aov_predictions)
+# * Confusion matrix
+confusionMatrix(as.factor(aov_predictions), as.factor(mydata$test), positive = NULL, dnn = c("Prediction", "Reference"))
+
 # Which regression did you use?
 # * ANCOVA, ANOVA, simple regression, logistic regression
 # * Justify your answer
