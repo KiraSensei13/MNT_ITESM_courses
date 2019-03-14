8de94b701757d53b1e9452813cde480596bc1420
Brux <bgonsor@protonmail.com>
2019-03-14 01:29:46 -0600

    The app is looking a little bit better I think it might be possible

--- 1.1.Computacion_Aplicada/MidtermExam/MidtermExam_ShinyApp/server.R
+++ 1.1.Computacion_Aplicada/MidtermExam/MidtermExam_ShinyApp/server.R
@@ -2,11 +2,11 @@
 
 shinyServer( function (input,output,session){
   
-########### THIS SECTION IS JUST FOR THE INSTRUCTIONS OF EACH PROBLEM ########
+########### THIS SECTION IS JUST FOR THE INSTRUCTIONS OF EACH PROBLEM ##########
   
   output$`Problem Instructions` <- renderText({
     
-    problemId <- input$`Exam Problems`
+    problemId <- input$`ExamProblems`
     
     if (problemId == "First Section - Problem 1 a)"){
       
@@ -55,53 +55,23 @@ shinyServer( function (input,output,session){
   })
     
   
-######## THIS SECTION IS TO ADD THE CODE (PROCEDURE) ###############################
+########### THIS SECTION IS TO ADD THE CODE (PROCEDURE) ########################
 
   output$`Code` <- renderPrint({
       
-    problemId <- input$`Exam Problems`
+    problemId <- input$`ExamProblems`
       
-##### Procedure for problem 1 ########################
+######## Procedure for problem 1 ########################
       
     if (problemId == "First Section - Problem 1 a)") {
-        
-    }
+      
+          }
     
     else{
       
     }
     
-    # First we define a variable to call for the database to be analized:
-    choco_data = read.csv("Chocolate.csv")
-    str(choco_data) # Verifying there is no repeated information.
-    summary(choco_data) # Exploring the data.
-    
-    # There is no need to filter our data since all variables are categorical, with the exceptions of "sugarpercent", "pricepercent" and "winpercent" but none of them have any row with a value of 0. The only information that causes irregularities is the competitors name.
-    
-    chocolate_labels <- choco_data[,1] # First column contains the names of candy and we define it as the label for each row.
-    choco_data <- choco_data[,-1] # Taking away the first column that contains the candy names.
-    
-    # Setting numeric values to categoric value:
-    choco_data$chocolate <- as.factor(choco_data$chocolate)
-    summary(choco_data)
-    
-    # Since the dependent variable "Chocolate" is a categorical variable, we have decided to use Logistic Regression:
-    mdl <- glm(chocolate ~ ., data = choco_data, family = "binomial")
-    summary(mdl)
-    
-    mdl <- glm(chocolate ~ fruity + caramel +  peanutyalmondy + nougat + crispedricewafer + hard + bar + pluribus + sugarpercent + pricepercent + winpercent, data = choco_data, family = "binomial")
-    summary(mdl) # Visualizing the model to determine the most significant variables.
-    
-    # Takng away all the not significative parameters we obtain the following model:
-    
-    mdl <- glm(chocolate ~ fruity + winpercent, data = choco_data, family = "binomial")
-    summary(mdl) # Visualizing the model to determine the most significant variables.
-    
-    # Lets see the performance of our beta coefficients:
-    predicted <- predict(mdl, choco_data[,], type = "response")
-    
-    #We assume that: all values less than 0.5 correspond to the first category, 0.5 or bigger correspond to the second category
-    glm_predicted = ifelse(predicted > 0.5, 1, 0)
+   
     
 ######## Procedure for problem 2 ##############################################
       
@@ -113,85 +83,84 @@ shinyServer( function (input,output,session){
         
     }
     
-####### Procedure for problem 3 ##############################################    
+######## Procedure for problem 3 ##############################################    
         
     if (problemId == "Second Section - Problem b)") {
-          
+      
+      library(pracma)
+      taylorPlot <- function(f, c, from, to) {
+        # Plot the Taylor approximations up to the 2nd, 4th, 6th and 8th terms
+        #
+        # Parameters
+        # ----------
+        # f : function
+        # Vectorized function of one variable
+        # c : numeric
+        # point where the series expansion will take place
+        # from, to : numeric
+        # Interval of points to be ploted
+        #
+        # Returns
+        # -------
+        # void
+        
+        x <- seq(from, to, length.out = 100)
+        yf <- f(x)
+        
+        yp2 <- polyval(taylor(f, c, 2), x)
+        yp4 <- polyval(taylor(f, c, 4), x)
+        yp6 <- polyval(taylor(f, c, 6), x)
+        yp8 <- polyval(taylor(f, c, 8), x)
+        
+        plot(
+          x,
+          yf,
+          xlab = "x",
+          ylab = "f(x)",
+          type = "l",
+          main = ' Taylor Series Approximation of f(x) ',
+          col = "black",
+          lwd = 2
+        )
+        
+        lines(x, yp2, col = "#c8e6c9")
+        lines(x, yp4, col = "#81c784")
+        lines(x, yp6, col = "#4caf50")
+        lines(x, yp8, col = "#388e3c")
+        
+        legend(
+          'topleft',
+          inset = .05,
+          legend = c("TS 8 terms", "TS 6 terms", "TS 4 terms", "TS 2 terms", "f(x)"),
+          col = c('#388e3c', '#4caf50', '#81c784', '#c8e6c9', 'black'),
+          lwd = c(1),
+          bty = 'n',
+          cex = .75
+        )
+      }
+      
+      
+      f0 <- function(x) {
+        res = sin(x)
+        
+        return(res)
+        
+      }
+      
+      f1 <- function(x) {
+        res = exp(complex(real = 0, imaginary = 1)*x)
+        
+        return(res)
+        
+      }
+        
     }
         
     else{
       
     }
     
-    library(pracma)
-    taylorPlot <- function(f, c, from, to) {
-      # Plot the Taylor approximations up to the 2nd, 4th, 6th and 8th terms
-      #
-      # Parameters
-      # ----------
-      # f : function
-      # Vectorized function of one variable
-      # c : numeric
-      # point where the series expansion will take place
-      # from, to : numeric
-      # Interval of points to be ploted
-      #
-      # Returns
-      # -------
-      # void
-      
-      x <- seq(from, to, length.out = 100)
-      yf <- f(x)
-      
-      yp2 <- polyval(taylor(f, c, 2), x)
-      yp4 <- polyval(taylor(f, c, 4), x)
-      yp6 <- polyval(taylor(f, c, 6), x)
-      yp8 <- polyval(taylor(f, c, 8), x)
-      
-      plot(
-        x,
-        yf,
-        xlab = "x",
-        ylab = "f(x)",
-        type = "l",
-        main = ' Taylor Series Approximation of f(x) ',
-        col = "black",
-        lwd = 2
-      )
-      
-      lines(x, yp2, col = "#c8e6c9")
-      lines(x, yp4, col = "#81c784")
-      lines(x, yp6, col = "#4caf50")
-      lines(x, yp8, col = "#388e3c")
-      
-      legend(
-        'topleft',
-        inset = .05,
-        legend = c("TS 8 terms", "TS 6 terms", "TS 4 terms", "TS 2 terms", "f(x)"),
-        col = c('#388e3c', '#4caf50', '#81c784', '#c8e6c9', 'black'),
-        lwd = c(1),
-        bty = 'n',
-        cex = .75
-      )
-    }
-    
-    # -----
-    
-    f0 <- function(x) {
-      res = sin(x)
-      
-      return(res)
-      
-    }
-    
-    f1 <- function(x) {
-      res = exp(complex(real = 0, imaginary = 1)*x)
-      
-      return(res)
-      
-    }
-    
-###### Procedure for Problem 4 ############################################
+######## Procedure for Problem 4 ############################################
           
     if (problemId == "Second Section - Problem c)"){
     
@@ -206,95 +175,76 @@ shinyServer( function (input,output,session){
   })
     
     
-########### THIS SECTION IS FOR THE PLOTS CREATED BY THE PROGRAM ############
+########### THIS SECTION IS FOR THE PLOTS CREATED BY THE PROGRAM ###############
     
   output$`Graphical Representation` <- renderPlot({
     
-    problemId <- input$`Exam Problems`
+    problemId <- input$`ExamProblems`
 
-############ Plot for Problem 1 #############################################
+############## Plot for Problem 1 ##############################################
     
     if (problemId == "First Section - Problem 1 a)") {
       
-      # First we define a variable to call for the database to be analized:
       choco_data = read.csv("Chocolate.csv")
-      str(choco_data) # Verifying there is no repeated information.
-      summary(choco_data) # Exploring the data.
-      
-      # There is no need to filter our data since all variables are categorical, with the exceptions of "sugarpercent", "pricepercent" and "winpercent" but none of them have any row with a value of 0. The only information that causes irregularities is the competitors name.
-      
-      chocolate_labels <- choco_data[,1] # First column contains the names of candy and we define it as the label for each row.
-      choco_data <- choco_data[,-1] # Taking away the first column that contains the candy names.
-      
-      # Setting numeric values to categoric value:
+            chocolate_labels <- choco_data[,1] 
+      choco_data <- choco_data[,-1] 
       choco_data$chocolate <- as.factor(choco_data$chocolate)
-      summary(choco_data)
-      
-      # Since the dependent variable "Chocolate" is a categorical variable, we have decided to use Logistic Regression:
       mdl <- glm(chocolate ~ ., data = choco_data, family = "binomial")
-      summary(mdl)
-      
-      mdl <- glm(chocolate ~ fruity + caramel +  peanutyalmondy + nougat + crispedricewafer + hard + bar + pluribus + sugarpercent + pricepercent + winpercent, data = choco_data, family = "binomial")
-      summary(mdl) # Visualizing the model to determine the most significant variables.
-      
-      # Takng away all the not significative parameters we obtain the following model:
-      
       mdl <- glm(chocolate ~ fruity + winpercent, data = choco_data, family = "binomial")
-      summary(mdl) # Visualizing the model to determine the most significant variables.
-      
-      # Lets see the performance of our beta coefficients:
       predicted <- predict(mdl, choco_data[,], type = "response")
-      
-      #We assume that: all values less than 0.5 correspond to the first category, 0.5 or bigger correspond to the second category
       glm_predicted = ifelse(predicted > 0.5, 1, 0)
+      
+      graph <- hist(glm_predicted, main = "Histogram of Chocolate Frequency in Candy", ylab = "Frequency of Trait", xlab = "Presence of Chocolate", ylim = c(0, 60), labels = TRUE)
+    
+    }
+    
+    else{
+      
+############## Plot for Problem 2 ##############################################
         
-    }
+      if (problemId == "Second Section - Problem a)") {
+      
+      }
     
-    else{
+      else{
+    
+############## Plot for problem 3 ##############################################
+    
+        if (problemId == "Second Section - Problem b)") {
       
-    }
-      
-    hist(glm_predicted, main = "Histogram of Chocolate Frequency in Candy", ylab = "Frequency of Trait", xlab = "Presence of Chocolate", labels = TRUE)
-
-################ Plot for Problem 2 ########################################
+        }
+    
+        else{
         
-    if (problemId == "Second Section - Problem a)") {
+############## Plot for problem 4 ##############################################
+    
+          if (problemId == "Second Section - Problem c)") {
       
-    }
+          }
     
-    else{
+          else{
+            
+            graph <- "Sorry for the inconvenience, we are still working hard to make this application evenn better."
+            
       
+          }
+    
+        }
+      }
     }
-    
-    
-    
-############## Plot for problem 3 ##########################################
-    
-    if (problemId == "Second Section - Problem a)") {
       
-    }
-    
-    else{
-      
-    }
-    
-    
-    
-############## Plot for problem 4 #########################################
-    
-    if (problemId == "Second Section - Problem a)") {
-      
-    }
-    
-    else{
-      
-    }
+      paste(graph)
     
   })
   
+########### THIS SECTION CORRESPONDS ONLY TO THE CHOCOLATE DATA ################  
+
   output$`Chocolate` <- renderTable({
+    
     choco_data = read.csv("Chocolate.csv")
+    
     choco_data
+    
   })
 
 }
--- 1.1.Computacion_Aplicada/MidtermExam/MidtermExam_ShinyApp/ui.R
+++ 1.1.Computacion_Aplicada/MidtermExam/MidtermExam_ShinyApp/ui.R
@@ -1,52 +1,72 @@
+######## INITIAL COMMENTS AND PREPARATIONS #####################################
+
 # Decided to develop an App in order to make the MidTerm exam more user Freindly.
 # Install the shiny package:
 # install.package("shiny")
 # Load Library:
 # library(shiny)
+# Giving some style:
+# install.packages("shinythemes")
+# Load Library:
+# library(shinythemes)
 
 shinyUI(
+  
+######## STYLE##################################################################
 # Defining the user interface:
   fluidPage(
     
-# Adding the Header:
+# Defining style:
+    theme = shinytheme("sandstone"),
     
-    titlePanel(h3("Midterm Exam", align = "center")),
-    titlePanel(h6("by: Antonio Osamu Katagiri Tanaka (A01212611) and Bruno Gonzalez Soria (A01169284)", align = "center")),
+######## HEADER OF THE APPLICATION #############################################
+    
+    titlePanel(h1("Midterm Exam", align = "center")),
+    titlePanel(h5("by: Antonio Osamu Katagiri Tanaka (A01212611) and Bruno Gonzalez Soria (A01169284)", align = "center")),
 
-# This is the side panel where the variables can be defined by the user to be evaluated by the program:  
+######## THIS IS THE SIDE PANEL FOR THE USER TO ENTER INPUT ####################  
   
     sidebarPanel(
-      selectInput("Exam Problems", "Please Select the problem to be evaluated:",
-                  choices=c("First Section - Problem 1 a)", "Second Section - Problem a)", "Second Section - Problem b)", "Second Section - Problem c)")),
+      selectInput("ExamProblems", "Please Select the problem to be evaluated:",
+                  choices = c("First Section - Problem 1 a)", "Second Section - Problem a)", "Second Section - Problem b)", "Second Section - Problem c)")),
       
-      conditionalPanel(condition = "input.Exam Problems == 'First Section - Problem 1 a)'"),
+      conditionalPanel(
+        condition = "input.ExamProblems == 'First Section - Problem 1 a)'"),
       
-      conditionalPanel(condition = "input.Exam Problems == 'Second Section - Problem a)'"),
+      conditionalPanel(
+        condition = "input.ExamProblems == 'Second Section - Problem a)'"),
       
-      conditionalPanel(condition = "input.Exam Problems == 'Second Section - Problem b)'",
-                       textInput("point", "Please enter the point x to which you want to approximate: ", 0),
-                       textInput("xmin", "Please enter lower value of x: ", -6),
-                       textInput("xmax", "Please enter upper value of x:", 6),
-                       sliderInput("factors", "Please select number of factors:", min = 1, max = 5000, value = 1, step = 1)),
+      conditionalPanel(
+        condition = "input.ExamProblems == 'Second Section - Problem b)'",
+        numericInput("point", "Please enter the point x to which you want to approximate: ", 0),
+        numericInput("xmin", "Please enter lower value of x: ", -6),
+        numericInput("xmax", "Please enter upper value of x:", 6),
+        sliderInput("factors", "Please select number of factors:",
+                    min = 1, max = 5000, value = 1, step = 1)),
       
-      conditionalPanel(condition = "input.Exam Problems == 'Second Section - Problem c)'")
+      conditionalPanel(
+        condition = "input.ExamProblems == 'Second Section - Problem c)'")
       
       
     ),
     
+####### THIS IS WHERE THE INFORMATION WILL BE SHOWN ############################
     mainPanel(
-      
+
+# Displaying the information in tabs:
       tabsetPanel(type="tab",
                   tabPanel("Solution",
                            tags$b(paste("Instructions:")),
-                           textOutput("Problem Instructions"),plotOutput("Graphical Representation")),
+                           textOutput("Problem Instructions"),
+                           plotOutput("Graphical Representation")),
                   tabPanel("Code", verbatimTextOutput("Code")),
-                  tabPanel("Chocolate Data", tableOutput("Chocolate"))
+                  tabPanel("Chocolate",
+                           tableOutput("Chocolate"))
                   )
       
+              )
+      
     )
-    
-  )
   
   
 )
