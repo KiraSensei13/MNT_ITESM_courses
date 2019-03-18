7ca830c8a44438b39b24202107c6c07cecb8b4e6
Osamu <oskatagiri@gmail.com>
2019-03-17 21:09:36 -0600

    new feature

--- 1.1.Computacion_Aplicada/MidtermExam/MidtermEvaluation.R
+++ 1.1.Computacion_Aplicada/MidtermExam/MidtermEvaluation.R
@@ -22,6 +22,11 @@
 # a) Dependent variable: Chocolate -> Chocolate is a categorical variable
 # Choose only the most significant variables to model the dependent variable. (10 points)
 
+cat("\014") # Clear console
+rm(list = ls(all = TRUE)) # Delete workspace
+
+# -----
+
 # First we define a variable to call for the database to be analized:
 choco_data = read.csv("Chocolate.csv")
 str(choco_data) # Verifying there is no repeated information.
@@ -60,9 +65,77 @@ hist(glm_predicted)
 # SECOND SECTION
 # a) Lagrange polynomials. This algorithm receives a nx2 matrix, where the first column represents the x coordinate while the second column represents the y coordinate. The code must provide as output the Lagrange polynomial interpolation expression in terms of "x". (30 points) *TIP: Use the functions: expression, D, parse and paste within a loop to get the desired output.
 
-library(deSolve)
-library(polynom)
-library(pracma)
+cat("\014") # Clear console
+rm(list = ls(all = TRUE)) # Delete workspace
+
+# -----
+
+getpoly <- function(coef = c(0, 1)) {
+  a <- as.numeric(coef)
+  while((la <- length(a)) > 1 && a[la] == 0) a <- a[-la]
+  structure(a, class = "polynomial")
+}
+
+# -----
+
+poly2char <- function(x, decreasing = FALSE, ...)
+{
+  p <- unclass(x)
+  lp <- length(p) - 1
+  names(p) <- 0:lp
+  p <- p[p != 0]
+  
+  if(length(p) == 0) return("0")
+  
+  if(decreasing) p <- rev(p)
+  
+  signs <- ifelse(p < 0, "- ", "+ ")
+  signs[1] <- if(signs[1] == "- ") "-" else ""
+  
+  np <- names(p)
+  p <- as.character(abs(p))
+  p[p == "1" & np != "0"] <- ""
+  
+  pow <- paste("x^", np, sep = "")
+  pow[np == "0"] <- ""
+  pow[np == "1"] <- "x"
+  stars <- rep.int("*", length(p))
+  stars[p == "" | pow == ""] <- ""
+  paste(signs, p, stars, pow, sep = "", collapse = " ")
+}
+
+# -----
+
+interpolatepoly <- function(x, y, tol = sqrt(.Machine$double.eps)) {
+  if(missing(y)) {
+    p <- 1
+    for(xi in x)
+      p <- c(0, p) - c(xi * p, 0)
+    return(getpoly(p))
+  }
+  
+  toss <- duplicated(x)
+  if(any(toss)) {
+    crit <- max(tapply(y, x, function(x) diff(range(x))))
+    keep <- !toss
+    y <- y[keep]
+    x <- x[keep]
+  }
+  
+  m <- length(x)
+  if(m <= 1)
+    return(getpoly(y))
+  
+  r <- 0
+  for(i in 1:m)
+    r <- r + (y[i] * unclass(Recall(x[ - i])))/prod(x[i] - x[ - i])
+  
+  r[abs(r) < tol] <- 0
+  getpoly(r)
+}
+
+# -----
+
 lagrange <- function(coordinates) {
   # Plot the Lagrange polynomial, evaluated within the x coordinates
   # (from parameter coordinates)
@@ -80,12 +153,20 @@ lagrange <- function(coordinates) {
   x = coordinates[,1]
   y = coordinates[,2]
   
-  interPoly = poly.calc(x,y)
-  xx = x
-  yy = lagrangeInterp(x,y,xx)
+  interPoly = interpolatepoly(x,y)
+  interPoly_str = poly2char(interPoly)
   
-  plot(xx, yy, xlab="x", ylab="f(x)", main = ' Lagrange polynomials ')
-  lines(interPoly, col = "#4caf50")
+  interpolation <- function(n) {
+    return(eval(parse(text=interPoly_str), envir=list(x=n)))
+  }
+  
+  yy = c()
+  for(i in 1:length(x)){
+    yy[i] = interpolation(x[i])
+  }
+  
+  plot(x, y, xlab="x", ylab="f(x)", main = ' Lagrange polynomials ')
+  lines(x, yy, col = "#4caf50")
   
   legend(
     'topleft',
@@ -97,7 +178,7 @@ lagrange <- function(coordinates) {
     cex = .75
   )
   
-  return(interPoly)
+  return(interPoly_str)
 }
 
 # -----
@@ -106,10 +187,58 @@ x = seq(-5,5,0.5)
 y = x^3 + 5*x^2 + 1
 lagrange(cbind(x,y))
 
+# -----
+
+# lagrange = function(x,y,a){
+#   n = length(x)
+#   if(a < min(x) || max(x) < a) stop("No está interpolando")
+#   X = matrix(rep(x, times=n), n, n, byrow=T)
+#   mN = a - X;
+#   diag(mN) = 1
+#   mD = X - t(X);
+#   diag(mD) = 1
+#   Lnk = apply(mN, 1, prod)/apply(mD, 2, prod)
+#   sum(y*Lnk)
+# }
+# 
+# lagrangePlot = function(x,y,step){
+#   xx = seq(min(x), max(x), step)
+#   yy = c()
+#   for (i in 1:length(xx)) {
+#     yy[i]=lagrange(x,y, xx[i])
+#   }
+#   plot(x, y, xlab="x", ylab="f(x)", main = ' Lagrange polynomials ')
+#   lines(xx, yy, col = "#4caf50")
+# }
+# 
+# # --- Prueba
+# x = seq(-5,5,2.5)
+# y = x^3
+# 
+# lagrangePlot(x,y,0.1)
+
 ################################################################################
 # b) Taylor series: The algorithm receives an expression or a string with the function to do and the number of terms to get. The output will be an expression containing all the Taylor series about 0. (30 points) *TIP: Use the functions: expression, D, parse and paste within a loop to get the desired output.
 
-library(pracma)
+cat("\014") # Clear console
+rm(list = ls(all = TRUE)) # Delete workspace
+
+# -----
+
+mytaylor <- function(f, c, taylorOrder = 4) {
+  fun <- match.fun(f)
+  f <- function(x) fun(x)
+  
+  T <- f(c)
+  library(pracma) # to add, derive, factorial and power a poly
+  for (i in 1:taylorOrder) {
+    T <- polyadd(T, fderiv(f, c, i)/fact(i) * polypow(c(1, -c), i))
+  }
+  return(T)
+}
+
+# -----
+
 taylorPlot <- function(funct, taylorOrder) {
   # Plot the Taylor approximations up to the taylorOrder terms
   #
@@ -135,7 +264,7 @@ taylorPlot <- function(funct, taylorOrder) {
   yf <- f(x)
   c <- 0 # The Taylor Series shall be centered in zero
   
-  taylorOut <- taylor(f, c, taylorOrder)
+  taylorOut <- mytaylor(f, c, taylorOrder)
   yp <- polyval(taylorOut, x)
   
   plot(
@@ -173,6 +302,11 @@ taylorPlot("tan(x)", 3)
 ################################################################################
 # c) Runge-Kutta. The algorithm will receive an ODE, initial values for x and y, the step size and the upper bound. The output will be the plot of the second, third and fourth order RK approximations. To demonstrate the functionality of your code, use the analytical answer of the ODE to compare all the approximations (30 points)
 
+cat("\014") # Clear console
+rm(list = ls(all = TRUE)) # Delete workspace
+
+# -----
+
 # Runge-Kutta - 2nd order
 rungeKutta2 <- function(funct, x0, y0, x1, n) {
   f <- function(xx,yy) {
@@ -209,7 +343,7 @@ rungeKutta3 <- function(funct, x0, y0, x1, n) {
   for(i in 1:n) {
     k1 <- h*f(x, y)
     k2 <- h*f(x + 0.5*h, y + 0.5*k1)
-    k3 <- h*f(x + 0.5*h, y + 0.5*k2)
+    k3 <- h*f(x + h, y + k2)
     vx[i + 1] <- x <- x0 + i*h
     vy[i + 1] <- y <- y + (k1 + 4*k2 + k3)/6
   }
@@ -305,7 +439,7 @@ RKPlot <- function(func, x0, y0, x1, n) {
   legend(
     'topleft',
     inset = .05,
-    legend = c("RK 4th order", "RK 3rd order", "RK 2nd order", "f'(x)"),
+    legend = c("RK 4th order", "RK 3rd order", "RK 2nd order", "deSolve ode function"),
     col = c('#2196f3', '#4caf50', '#f44336', 'black'),
     lwd = c(1),
     bty = 'n',
@@ -315,8 +449,8 @@ RKPlot <- function(func, x0, y0, x1, n) {
 
 # -----
 
-funct           = "x^2 - y^2"
-init_y          = 5
+funct           = "x^2 - y"
+init_y          = 1
 init_x          = -5
 upper_bound     = 5
 number_of_steps = length(init_x:upper_bound)*2
