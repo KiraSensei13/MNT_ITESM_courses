41dd92571f5bbf8486564e4a7e9f11871d8127fe
Brux <bgonsor@protonmail.com>
2019-04-10 08:18:21 -0500

    Checking HW1 and starting HW2

--- 1.1.Computacion_Aplicada/10_Optimization/HW02.m
+++ 1.1.Computacion_Aplicada/10_Optimization/HW02.m
@@ -18,7 +18,17 @@
 % ************************************************************************
 
 clc;
-clear all;
+% clear all;
+
+% syms -> Short-cut for constructing symbolic variables.
+% diff -> Differentiates a symbolic expression.
+% solve -> Symbolic solution of algebraic equations.
+% subs -> Symbolic substitution
+% fminunc -> Unconstrained minimization. Finds a local minimum of a function of several variables.
+% fminbnd -> Single-variable bounded nonlinear function minimization. Finds a local minimal in the
+% interval -> xmin < x < xmax.
+% fmincon -> Finds a constrained minimum of a function of several variables.
+% fminsearch -> Multidimensional unconstrained nonlinear minimization (Nelder-Mead).
 
 %% ************************************************************************
 % Problem 1:
@@ -29,6 +39,10 @@ clear all;
 % Upload to Blackboard a pdf file that contains a MATLAB script, any MATLAB
 % functions that you implemented, and required results.
 
+objective = (4 - 2.1 * x1^2 + x1^4/3) * x1^2 + x1 * x2 + (-4 + 4 * x2^2) * x2^2;
+x0 = [rand*15-5 rand*15]';
+xmin = fmincon(objective,x0,[],[],[],[],[-3 3]',[-2 2]');
+print xmin
 
 %% ************************************************************************
 % Problem 2:
@@ -41,3 +55,44 @@ clear all;
 % for x_1 [-5; 10] y x_2 [0; 15].
 % Upload to Blackboard a pdf file that contains a MATLAB script, any MATLAB
 % functions that you implemented, and required results.
+
+function y = branin(xx)
+% Branin's function
+x1 = xx(1);
+x2 = xx(2);
+t = 1 / (8*pi);
+s = 10;
+r = 6;
+c = 5/pi;
+b = 5.1 / (4*pi^2);
+a = 1;
+term1 = a * (x2 - b*x1.^2 + c*x1 - r).^2;
+term2 = s*(1-t)*cos(x1);
+y = term1 + term2 + s;
+end
+
+function y = branin2(x1,x2)
+% Branin's function (used for plotting)
+t = 1 / (8*pi);
+s = 10;
+r = 6;
+c = 5/pi;
+b = 5.1 / (4*pi^2);
+a = 1;
+term1 = a * (x2 - b*x1.^2 + c*x1 - r).^2;
+term2 = s*(1-t)*cos(x1);
+y = term1 + term2 + s;
+end
+
+%% Solution using fminsearch
+x0 = [rand*15-5 rand*15]';
+xmin = fmincon(@branin,x0,[],[],[],[],[-5 0]',[10 15]');
+branin(xmin)
+
+%% Branin's function graph
+[X1,X2] = meshgrid(linspace(-5,10),linspace(0,15));
+F = branin2(X1,X2);
+surf(X1,X2,F)
+shading interp
+xlabel('x_1')
+ylabel('x_2')
