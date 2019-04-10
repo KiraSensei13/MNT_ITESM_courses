bef030f5b4a601d2c8e217b15648a65076ffff17
Osamu <oskatagiri@gmail.com>
2019-04-10 14:32:39 -0500

    merge HW02

--- 1.1.Computacion_Aplicada/10_Optimization/HW02.m
+++ 1.1.Computacion_Aplicada/10_Optimization/HW02.m
@@ -35,7 +35,27 @@ close all;
 % Upload to Blackboard a pdf file that contains a MATLAB script, any MATLAB
 % functions that you implemented, and required results.
 
+objective = (4 - 2.1 * x1^2 + x1^4/3) * x1^2 + x1 * x2 + (-4 + 4 * x2^2) * x2^2;
+x0 = [-2,5 2.5]';
+disp(['Initial Objective: ' num2str(objective(x0))])
 
+function [c, ceq] = nlcon(x)
+    c = 2;
+    ceq = 4;
+end
+
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+lb = [-3 -2]';
+ub = [3 2]';
+nonlcon = @nlcon;
+
+[x, fval, ef, output, lambda] = fmincon(objective, x0, A, b, Aeq, beq, lb, ub, nonlcon);
+
+disp(x)
+disp(['Final Objective: ' num2str(objective(x))])
 
 %% ************************************************************************
 % Problem 2:
@@ -59,4 +79,43 @@ t = 1/(8*pi);
 % Upload to Blackboard a pdf file that contains a MATLAB script, any MATLAB
 % functions that you implemented, and required results.
 
+% function y = branin(xx)
+% % Branin's function
+% x1 = xx(1);
+% x2 = xx(2);
+% t = 1 / (8*pi);
+% s = 10;
+% r = 6;
+% c = 5/pi;
+% b = 5.1 / (4*pi^2);
+% a = 1;
+% term1 = a * (x2 - b*x1.^2 + c*x1 - r).^2;
+% term2 = s*(1-t)*cos(x1);
+% y = term1 + term2 + s;
+% end
 
+% function y = branin2(x1,x2)
+% Branin's function (used for plotting)
+% t = 1 / (8*pi);
+% s = 10;
+% r = 6;
+% c = 5/pi;
+% b = 5.1 / (4*pi^2);
+% a = 1;
+% term1 = a * (x2 - b*x1.^2 + c*x1 - r).^2;
+% term2 = s*(1-t)*cos(x1);
+% y = term1 + term2 + s;
+% end
+
+%% Solution using fminsearch
+% x0 = [rand*15-5 rand*15];
+% xmin = fmincon(@branin,x0,[],[],[],[],[-5 0]',[10 15]');
+% branin(xmin)
+
+%% Branin's function graph
+% [X1,X2] = meshgrid(linspace(-5,10),linspace(0,15));
+% F = branin2(X1,X2);
+% surf(X1,X2,F)
+% shading interp
+% xlabel('x_1')
+% ylabel('x_2')
