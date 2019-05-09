6d24cdb25557915fa58bb9014d303f270f4fd5b0
Osamu <oskatagiri@gmail.com>
2019-05-09 14:25:27 -0500

    cLearning DONE

--- 1.1.Computacion_Aplicada/Final Exam/cLearning.m
+++ 1.1.Computacion_Aplicada/Final Exam/cLearning.m
@@ -11,9 +11,9 @@
 % *     Final Exam
 % *
 % * NOTES :
-% *     In submitting the solution to this final exam, I (we) your name(s)
-% *     affirm my (our) awareness of the standards of the Tecnológico de
-% *     Monterrey Ethics Code.
+% *     In submitting the solution to this final exam, We Bruno González
+% *     Soria and Antonio Osamu Katagiri Tanaka affirm our awareness of the
+% *     standards of the Tecnológico de Monterrey Ethics Code.
 % *
 % *     Thanks to The Coding Train: https://www.youtube.com/watch?v=XJ7HLz9
 % *     VYz0&list=PLRqwX-V7Uu6Y7MdSCaIfsxc561QI0U0Tb&index=1
@@ -23,9 +23,9 @@
 % ************************************************************************
 
 close all, clear all, clc, format compact
+rng(31416)
 
-%% ************************************************************************
-% Problem 3: LEARNING
+%% * Problem 3: LEARNING **************************************************
 % The age of a specific species of shellfish is related to several physical
 % characteristics. The sheet data of the Excel file shellfish.xlsx contains
 % data of 4077 individuals, and their ages
@@ -45,6 +45,25 @@ close all, clear all, clc, format compact
 % Age            | integer    |       | years
 % ---------------+------------+-------+----------------------------
 
+%% ************************************************************************
+% a) Train a neural network using the information of these 4077.
+%     1) This data must be divided into training, testing, and possibly
+%        validation examples. Explain your decision when choosing these
+%        data sets.
+%
+%        The neural netowrk (AKA brain) is trained through a superviced
+%        learning algorithm. Therefore the 4077 entries in shellfish.xlsx
+%        are used for both purposes, training and testing. The reason
+%        behind this desition is to consider the most amount of entries as
+%        possible, therfore to train the brain with all the known data.
+%
+%     2) Explain any pre-processing done to the data.
+%
+%        Two steps were performed to pre-process the data: 1) convert the
+%        imported cell-matrix data types into ordinary matrices, and
+%        2) convert the categorical variables into integer values. These
+%        convertions are done to ease the algorithm computation.
+
 % load shellfish data
 ssds = spreadsheetDatastore('./shellfish.xlsx');
 
@@ -55,27 +74,20 @@ data_arr = zeros(height(data),width(data));
 
 % convert cell matrix to ordinary matrix
 for k=1:width(data)
-    data_varNames = data.Properties.VariableNames(k);
-    data_varNames = cell2mat(data_varNames);
+   %data_varNames = data.Properties.VariableNames(k); %debuggin purposes
+   %data_varNames = cell2mat(data_varNames); %debuggin purposes
     table_col = table2array(data(1:height(data),k));
     
     if isa(table_col,'cell')
-        % convert data.Sex values to integers:
+        % convert categorical values to integers:
         % F = 70, M = 77, I = 73
         data_arr(1:height(data),k) = cell2mat(table_col);
-    else
+    else % keep the values as they are
         data_arr(1:height(data),k) = table_col;
     end
 end
 
 % data_arr shall be used for the neural training
-
-%% ************************************************************************
-% a) Train a neural network using the information of these 4077.
-%     1) This data must be divided into training, testing, and possibly
-%        validation examples. Explain your decision when choosing these
-%        data sets.
-%     2) Explain any pre-processing done to the data.
 
 % let's train the brain/perceptron with the known training data in
 % data_arr - Through a SUPERVISED LEARNING ALGORITHM
@@ -87,27 +99,26 @@ end
 %    5) Return to step 1) and repeat!
 
 % initialize the weights randomly
-nInputs = width(data)-1;
+nInputs = width(data)-1; % number of inputs
 weights = zeros(1,nInputs);
 for k=1:nInputs
-    weights(1,k) = randi([-8 8]);
+    weights(1,k) = randi([-10 10]); % random numbers between -10 and 10
 end
 % from previous trainings, the weights are estimated to be the following:
 weights = [-7.5340 5.6109 5.3689 6.3149 2.7319 -10.4802 7.2560 0.1472];
 
 % let's create some variables to see how well brain is being trained
-guess = zeros(1,length(data_arr));
-known = zeros(1,length(data_arr));
+guess = zeros(1,length(data_arr)); % to store what the perceptron guesses
+known = zeros(1,length(data_arr)); % to store the correct answers
 
-% let's train the brain n-times
-n = 10;
+n = 25; % let's train the brain n-times
 for i=1:n
     for k=1:length(data_arr)
-        % let's feed our brain/perceptron some intputs to get a guess.
+        % let's feed our brain/perceptron some intputs
         inputs = data_arr(k,1:width(data)-1);
-        % set the known target to compute the error
+        % set the known target (correct answer) to compute the error
         target = data_arr(k,width(data));
-        % let's create a brain
+        % let's create a brain/neuron
         brain = perceptron(inputs,weights,target);
         % let's ask brain for a guess
         brain = brain.guess;
@@ -121,12 +132,16 @@ for i=1:n
     end
 end
 
+% Guesses are rounded to the nearest integer. Age is od type integer, as
+% defined in shellfish.xlsx data description
 guess = round(guess);
 
 %% ************************************************************************
 % b) Using your trained neural network, determine the age of the 100
 %    individuals in sheet predict. Write the results to as a column of an
 %    Excel worksheet.
+%
+%    The predicted data (ages) are stored in predictedAge
 
 % store the 2nd sheet - PREDICT *******************************************
 ssds.Sheets = 2;
@@ -135,15 +150,15 @@ predict_arr = zeros(height(predict),width(predict));
 
 % convert cell matrix to ordinary matrix
 for k=1:width(predict)
-    predict_varNames = predict.Properties.VariableNames(k);
-    predict_varNames = cell2mat(predict_varNames);
+   %predict_varNames = predict.Properties.VariableNames(k); %debuggin purposes
+   %predict_varNames = cell2mat(predict_varNames); %debuggin purposes
     table_col = table2array(predict(1:height(predict),k));
 
     if isa(table_col,'cell')
-        % convert data.Sex values to integers:
+        % convert categorical values to integers:
         % F = 70, M = 77, I = 73
         predict_arr(1:height(predict),k) = cell2mat(table_col);
-    else
+    else % keep the values as they are
         predict_arr(1:height(predict),k) = table_col;
     end
 end
@@ -163,27 +178,42 @@ for k=1:length(predict_arr)
     brain = perceptron(inputs,weights,target);
     % let's ask brain for a guess
     brain = brain.guess;
-    % populate the tracking variables
+    % populate the tracking variable
     predictedAge(k,1) = brain.Output;
 end
 
+% Predictions are rounded to the nearest integer. Age is od type integer,
+% as defined in shellfish.xlsx data description
 predictedAge = round(predictedAge);
 
 % Save predictions into a CSV file
 tbl = [predict array2table(predictedAge)];
-writetable(tbl,'./predictions.csv','WriteRowNames',true,'Delimiter',','); 
+writetable(tbl,'./cLearning.csv','WriteRowNames',true,'Delimiter',',');
+disp(tbl)
 
 %% ************************************************************************
 % c) Give an estimate of the expected error of your neural network on new
 %    data. Explain your answer.
+%
+%    The espected relative error is around 13% (as calculated with
+%    mean(error)). We expect a single neural network to get things right
+%    every time, however that's not how the brain works. The firsst neural
+%    network gets things wrong often. If we trained an artificial neural
+%    network to reduce the error, we would need to slow the learning rate
+%    early (learningRate = 0.00001), and halt long before over-fitting
+%    (n = 25). However the 1st network would still get many answers wrong.
+%    Then, we would need a second neural network, that receives the 1st
+%    network mistakes to drop the error significantly.
 
 % let's create more variables to see how well brain is trained
-error = known - guess; % error shall be close to zero ...
+error = abs(guess - known)./known.*100; % error shall be close to zero ...
 x = 1:length(error);
+% scatter the points with blue filled dots
 scatter(x,error,3,[0 0.4470 0.7410],'filled');
-title("Scatter Plot of the Predictions' Error with "+n+" Trains");
-xlabel('Predictions');
-ylabel('Error = Prediction - Target');
-txt = strcat('mean(error) = ', num2str(mean(error)));
-text(0,max(error)-1,txt) % print the error's mean into the chart
-
+title("Scatter Plot of the Training Relative-Error with "+n+" Trains");
+xlabel('Guesses');
+xlim([0 length(error)]);
+ylabel('relativeError = (Guess - Target)/Target');
+ylim([-15 115]);
+txt = strcat('mean(relativeError) = ', num2str(mean(error)), '%');
+text(0,max(error)-2,txt) % print the error's mean into the chart
